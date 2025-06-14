44d29d8289f55552added8de0703192c39b75cf9
SaiSri-bit <sriedu101@gmail.com>
2025-06-14 19:21:20 -0400

    Added documentation for the existing group class

--- categories/Group.py
+++ categories/Group.py
@@ -1,7 +1,13 @@
 from Set import Set, function
 from Category import Object,Morphism
 
+# A Group as an object caan be viewed as a set followed with an operation that follow as a certain
+# set of laws described below. To implement this, we should first define what an operation can be
+
+
 class Operation:
+    # For the case of simplicity, any operations should be a callable function. This will allow
+    # us to be able to conduct operations within the group
     def __init__(self, op_func):
         if not callable(op_func):
             raise TypeError("Operation must be a callable function.")
@@ -13,20 +19,34 @@ class Operation:
         
 class grp(Object):
     def __init__(self, elements:Set, identity, operation:Operation ):
-        self.X = elements
-        self.operation = operation
+        # Firstly, confirm that the identity element is part of the set
         if not (elements.contains(identity)):
             raise TypeError("Identity Element is missing from set")
+        
+        # Then store the appropariate variables
+        self.X = elements # The set of elements is the main thing being stored
+        self.operation = operation # Store the operation as another variable to be called later
+        self.identity = identity # The identity element is one of the most important elements and should be stored seperately
+
+        # After storing the above variables, we should also confirm whether or not the set and operation
+        # will form a group by making sure the definition of the groups are met. 
         if not self.verify_group(identity):
             raise TypeError ("One of the verifications failed. Set and Operation do not form group")
-        self.identity = identity
 
 
     def verify_group(self,identity):
+        # There are 4 main criteria that a set and operation pair have to meet to be considered to be a group:
+        # 1. The set must be closed (the operation can only transmute one element of the set to another element 
+        # that is in that same set)
+        # 2. The operation must be associative (a*b)*c = a*(b*c)
+        # 3. The set must contain a unique identity element such that a*e = a
+        # 4. The set must contain a unque inverse element such taht a*a^{-1}= e
         return self.verify_associative() and self.verify_identity(identity) and self.verify_inverse(identity) and self.verify_closed()
     
 
     def verify_associative(self):
+        # This method parses through every possible three element combination and makes sure that associativity
+        # is kept within the group with the operation
         for element1 in self.X:
             for element2 in self.X:
                 for element3 in self.X:
@@ -36,6 +56,8 @@ class grp(Object):
 
 
     def verify_identity(self,identity):
+        # This just goes ahead and insures that the element when operated with itself and the identity element
+        # results in the starting element for all elements in the set
         for element in self.X:
             if not (self.operation.apply(element, identity) == element and self.operation.apply(identity, element) == element):
                 return False
@@ -43,6 +65,8 @@ class grp(Object):
     
 
     def verify_inverse(self,identity):
+        # This method goes through every element, and makes sure that there exists some valid element such that
+        # when both elements are combined under the given operation, the identity element is achieved
         for element1 in self.X:
             check = False
             count = 0
@@ -55,6 +79,8 @@ class grp(Object):
 
 
     def verify_closed(self):
+        # This method insures that all possible combinations of elements under the given operation will result
+        # in another element that is within the existing set
         for a in self.X:
             for b in self.X:
                 if self.operation.apply(a, b) not in self.X:
@@ -63,10 +89,13 @@ class grp(Object):
     
 
     def getIdentity(self):
+        # We stored the identity earlier for a faster return
         return self.identity
 
 
     def find_Inverse(self,element):
+        # Similar to verification, we just need go through each combinations of elements until one combines
+        # to the identity element at which point we return that intitial element
         check = False
         count = 0
         while not check:
@@ -78,6 +107,8 @@ class grp(Object):
                 
 
     def check_if_abeliean(self):
+        # Abelian groups are groups that have a fifth property of commutativity and the method checks and
+        # returns true or false based on if its abelian or not.
         for element1 in self.X:
             for element2 in self.X:
                 if self.operation.apply(element1,element2) != self.operation.apply(element2,element1):
