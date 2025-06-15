769bcc5832f9869e015f20345a36754c9e223fa1
SaiSri-bit <sriedu101@gmail.com>
2025-06-15 00:15:51 -0400

    Added a verfication process to the top class and added appropriate documentation. Went ahead and also updated README. Specifically, "elements" will be used as the variable name when refering to general sets to avoid conflict with either class names.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GeneralSetEngine
 A generalized set engine that handles and allows operations on set based mathematical objects like groups, topological spaces, measurable spaces, and graphs.
+
+
+Generally "elements" is used as a variable in place of the phrase of set to prevent any possible complication between the class set and a variable of the same name.
--- categories/TopologicalSpace.py
+++ categories/TopologicalSpace.py
@@ -1,7 +1,38 @@
-from Set import set, function
+from Set import Set, function
 
 class top(object):
-    pass
+    def __init__(self,elements:Set, topology:Set):
+        self.X = elements # The underlying set should saved seperately from the topology
+        self.topology = topology # The topology which the feature added by a topological space should be stored
+        if not self.verifyTopology():
+            raise ValueError("Topology not valid for given underlying set")
+        
+    def verifyTopology(self):
+        # Check to make sure that the empty set is within the topology
+        if [] not in self.topology:
+            print ("Empty Set not in topology")
+            return False
+        
+        # Check to see the entire underlying set is in the topology
+        if [].append(self.X) not in self.topology:
+            print ("Underlying set is not within the topology")
+            return False
+
+        # Check to see if the topology is closed under the union operation
+        for subset_a in self.topology:
+            for subset_b in self.topology:
+                if list(set(subset_a).union(set(subset_b))) not in self.topology:
+                    print("Topology is not closed under union")
+                    return False
+                
+        # Check to see if the topology is closed under the intersection operation
+        for subset_a in self.topology:
+            for subset_b in self.topology:
+                if list(set(subset_a).intersection(set(subset_b))) not in self.topology:
+                    print("Topology is not closed under finite intersection")
+                    return False
+
+        return True
 
 class continuousFunction(function):
     pass
