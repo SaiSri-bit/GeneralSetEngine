523a59e27555141f7cb373bd9eece788cd120cc4
SaiSri-bit <sriedu101@gmail.com>
2025-06-19 18:49:25 -0400

    Added in the comments for class placements in the test_categories file. Classes should be tested for each method. Plan is to do this saturday.

--- testing/test_categories.py
+++ testing/test_categories.py
@@ -7,4 +7,49 @@ from categories.TopologicalSpace import top
 from categories.MeasurableSpace import meas
 
 class TestCategories(unittest.TestCase):
+    ## Category Module Tests
+
+    ### Object Class Tests
+
+    ### Morphism Class Tests
+
+    ### Category Class Tests
+
+    ### IdentityMorphism Class Tests
+
+    ### Functors Class Tests
+
+
+
+    ## Set Module Tests 
+
+    ### Set Class Tests
+
+    ### Function Class Tests
+
+
+
+    ## Graph Module Tests
+
+    ### Graph Class Tests
+
+
+
+    ## Group Module Tests
+ 
+    ### Operation Class Tests
+ 
+    ### Grp Class Tests
+
+
+
+    ## TopologicalSpace Module Tests
+
+    ### Top class tests
+
+
+
+    ## MeasurableSpace Module Tests
+
+    ### Meas Class Tests
     pass
