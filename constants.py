e20970a44a5816436b8f7ce1ec3ae8214e181540
SaiSri-bit <sriedu101@gmail.com>
2025-06-21 16:41:21 -0400

    Added an embedding file which will deal with positional encoding and all. Defined basic trig values using the taylor series expansions. This will allow us to escape using the math libraries to approximate values that use these functions.

--- constants.py
+++ constants.py
@@ -1,2 +1,31 @@
 pi = 3.141592653589793
 e = 2.718281828459045
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+def sin(x, terms=10):
+    result = 0
+    for n in range(terms):
+        term = ((-1)**n) * (x**(2*n + 1)) / factorial(2*n + 1)
+        result += term
+    return result
+
+def cos(x, terms=10):
+    result = 0
+    for n in range(terms):
+        term = ((-1)**n) * (x**(2*n)) / factorial(2*n)
+        result += term
+    return result
+
+def exp(x, terms=10):
+    result = 0
+    for n in range(terms):
+        term = (x**n) / factorial(n)
+        result += term
+    return result
